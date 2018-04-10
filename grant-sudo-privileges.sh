ccc74bbcfd6102c38e1471152ebee9068cf737e0
Jul <julienbenjamin.ruiz@protonmail.com>
2018-04-10 14:30:32 +0200

    add root checking

--- grant-sudo-privileges.sh
+++ grant-sudo-privileges.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-echo "What is the user you wish to grant sudo privileges with?"
-echo "Enter username: "
-read -e USERNAME # TODO: implement whether a user exists or not
-usermod -a -G sudo $USERNAME
+# Script to grant sudo privileges to specific user
+if [ $(id -u) -eq 0 ]; then
+    echo "What is the user you wish to grant sudo privileges with?"
+    echo "Enter username: "
+    read -e USERNAME # TODO: implement whether a user exists or not
+    usermod -a -G sudo $USERNAME
+else
+	echo "Only root may grant sudo privileges to a user"
+	exit 2
+fi

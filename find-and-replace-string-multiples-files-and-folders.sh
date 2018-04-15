c2b0510e3ef9109eb2ae6699d916e8fe093b47d3
Jul <julienbenjamin@protonmail.com>
2018-04-15 15:27:20 +0200

    add find-and-replace-string-multiples-files-and-folders.sh script

---
+++ find-and-replace-string-multiples-files-and-folders.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script will search for the string 'old' in all files
+# relative to the passed directory and replace 'old'
+# with 'new' for each occurrence of the string in each file.
+#
+
+DIRECTORY=$1
+OLD_STR=$1
+NEW_STR=$2
+
+grep -rl $OLD_STR $DIRECTORY | xargs sed -i "s/${OLD_STR}/${NEW_STR}/g"
